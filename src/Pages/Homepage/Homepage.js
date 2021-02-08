ebbefbc1c65104956cdf4177bb56ecd85375bac6
anand simmy <anand.simmy@razorthink.com>
2021-02-08 14:09:35 +0530

    added section 4 and 5

--- src/Pages/Homepage/Homepage.js
+++ src/Pages/Homepage/Homepage.js
@@ -8,7 +8,7 @@ const Homepage= () => {
         <div className={styles.mainContainer}>
             <div className={styles.navbar}>
                 <div className={styles.logo}>
-                    <img src={'./img/rzt-logo.svg'} />
+                    <img src={'./img/homepage/rzt-logo.svg'} />
                 </div>
                 <div className={styles.navLinksContainer}>
                     <ul className={styles.navLinks}>
@@ -20,8 +20,8 @@ const Homepage= () => {
                     </ul>
                 </div>
             </div>
-            <div className={`${styles.section1} ${styles.heroBanner}`}>
-                <img src={'./img/hero.png'} />
+            <div className={styles.section1}>
+                <img src={'./img/homepage/hero.png'} />
                 <div className={styles.heroBannerContent}>
                     <div className={styles.heroBannerText}>
                         <span className={styles.heroBannerTextOne}>Improve Your Company Performance with</span>
@@ -49,7 +49,7 @@ const Homepage= () => {
                         <button className={styles.learnMoreButton}>Learn More</button>
                     </div>
                     <div className={styles.imageContainer}>
-                        <img src={'./img/section2.png'} />
+                        <img src={'./img/homepage/section2.png'} />
                     </div>
                 </div>
             </div>
@@ -65,31 +65,31 @@ const Homepage= () => {
                     </div>
                     <div className={styles.pipelineContainer}>
                         <Card
-                            logoSrc='./img/card-images/create-1.png'
+                            logoSrc='./img/homepage/card-images/create-1.png'
                             heading='Create'
                             info='Pluggable Components Visual Tools <-> SDK Collaboration'
                         />
                         <div className={styles.arrowContainer}>
-                            <img alt='arrow' src='./img/card-images/arrow-2.svg' />
+                            <img alt='arrow' src='./img/homepage/card-images/arrow-2.svg' />
                         </div>
                         <Card
-                            logoSrc='./img/card-images/tune.png'
+                            logoSrc='./img/homepage/card-images/tune.png'
                             heading='Tune'
                             info='Scaled Experiments Versioning (Code, Artifacts and Data) Explanations'
                         />
                         <div className={styles.arrowContainer}>
-                            <img alt='arrow' src='./img/card-images/arrow-2.svg' />
+                            <img alt='arrow' src='./img/homepage/card-images/arrow-2.svg' />
                         </div>
                         <Card
-                            logoSrc='./img/card-images/Deploy.png'
+                            logoSrc='./img/homepage/card-images/Deploy.png'
                             heading='Deploy'
                             info='Single Click Deployment Workflow Seamless Integration'
                         />
                         <div className={styles.arrowContainer}>
-                            <img alt='arrow' src='./img/card-images/arrow-2.svg' />
+                            <img alt='arrow' src='./img/homepage/card-images/arrow-2.svg' />
                         </div>
                         <Card
-                            logoSrc='./img/card-images/Operate-1.png'
+                            logoSrc='./img/homepage/card-images/Operate-1.png'
                             heading='Operate'
                             info='Performance and Fault Tolerance Governance Model Monitoring'
                         />
@@ -97,6 +97,88 @@ const Homepage= () => {
                     <div className={styles.trainButton}>
                         <button className={styles.trainYourOwnButton}>Train your own</button>
                     </div>
+                </div>
+            </div>
+            <div className={styles.section4}>
+                <div className={styles.container}>
+                    <div className={styles.headingContainer}>
+                        <div className={styles.heading}>
+                            <span>The RazorThink</span>
+                            <span>Advantage</span>
+                        </div>
+                    </div>
+                    <div className={styles.advantageContainer}>
+                        <div className={styles.advantages}>
+                            <div className={styles.advantagesSection1}>
+                                <div className={styles.advantagesBox}>
+                                    <div className={styles.advantagesBoxContent}>
+                                        Intelligent Systems and Processes
+                                    </div>
+                                </div>
+                                <div className={styles.advantagesBox}>
+                                    <div className={styles.advantagesBoxContent}>
+                                        Easy to use, highly flexible
+                                    </div>
+                                </div>
+                                <div className={styles.advantagesBox}>
+                                    <div className={styles.advantagesBoxContent}>
+                                        Visibility
+                                    </div>
+                                </div>
+                                <div className={styles.advantagesBox}>
+                                    <div className={styles.advantagesBoxContent}>
+                                        Scalability of AI development can be achieved
+                                    </div>
+                                </div>
+                            </div>
+                            <div className={styles.advantagesSection2}>
+                                <div className={styles.advantagesBox}>
+                                    <div className={styles.advantagesBoxContent}>
+                                        Single operating system includes all current and future AI
+                                    </div>
+                                </div>
+                                <div className={styles.advantagesBox}>
+                                    <div className={styles.advantagesBoxContent}>
+                                        Hosting flexibility
+                                    </div>
+                                </div>
+                                <div className={styles.advantagesBox}>
+                                    <div className={styles.advantagesBoxContent}>
+                                        Preserve your current AI investment
+                                    </div>
+                                </div>
+                                <div className={styles.advantagesBox}>
+                                    <div className={styles.advantagesBoxContent}>
+                                        Versioning is automated
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div className={styles.advantageImage}>
+                            <img alt='advantages' src={'./img/homepage/image 2.png'} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={styles.section5}>
+                <div className={styles.container}>
+                    <div className={styles.headingContainer}>
+                        <div className={styles.heading}>
+                            Get started today
+                        </div>
+                        <div className={styles.headingCaption}>
+                            Are you ready to experience the power and
+                            simplicity of market proven AI Products?
+                        </div>
+                        <div className={styles.requestButton}>
+                            <button>REQUEST A DEMO</button>
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.upwardButton}>
+                    <a href='#'>
+                        <img alt='arrow' src='./img/homepage/arrow.svg' />
+                    </a>
                 </div>
             </div>
         </div>
